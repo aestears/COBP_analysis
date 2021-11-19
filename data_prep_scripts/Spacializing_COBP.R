7bc8bee6e81607c88f8c8c87e0b0756f4182767b
Alice Stears <alice.e.stears@gmail.com>
2021-11-19 13:08:39 -0700

    spatialized 'long-form' data from the data prep script

--- data_prep_scripts/Spacializing_COBP.R
+++ data_prep_scripts/Spacializing_COBP.R
@@ -1,7 +1,10 @@
 library(tidyverse)
 library(sf)
+## load the data from "preppingData.R"
+source("./data_prep_scripts/preppingData.R")
 ## convert the data to an sf format
 ## first make them points
+
 butterfly<- st_as_sf(butterfly, coords = c("X_cm", "Y_cm"))
 ## add a buffer with a radius of .5 cm 
 butterfly <- st_buffer(butterfly, dist = .5)
@@ -19,6 +22,6 @@ ggplot(butterfly[butterfly$Plot_ID == "S9",]) +
   theme_classic()
 
 
-rm(list = (ls()[2:12]))
-## save a version of the spatialized data
+rm(list = (ls()[c(1,3:21)]))
+## save a version of the environment (to avoid the )
 save.image(file = "../../Oenothera coloradensis project/Processed_Data/spatial_COBP.RData")
