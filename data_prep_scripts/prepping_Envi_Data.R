574808be81f00cab4d2bf7877b1d5229dfc24f9d
aestears <astears@uwyo.edu>
2023-02-02 14:32:29 -0700

    mostly making file paths more generic

--- data_prep_scripts/prepping_Envi_Data.R
+++ data_prep_scripts/prepping_Envi_Data.R
@@ -5,9 +5,10 @@
 #//////////////////////////////////////////////////
 
 library(tidyverse)
-
+# location of data files 
+datFolder <- "/Users/alicestears/Dropbox/Grad School/Research/Oenothera coloradensis project"
 #### load soil moisture data ####
-soilMoist <- read.csv("../Raw Data/EnviData/SoilMoisture.csv")
+soilMoist <- read.csv(paste0(datFolder,"/Raw Data/EnviData/SoilMoisture.csv"))
 # subset for m3/m3 data
 soilMoist <- soilMoist %>% 
   filter(Reading_Type == "m3/m3") %>% 
@@ -15,7 +16,7 @@ soilMoist <- soilMoist %>%
   summarize(SoilMoisture_m3m3 = mean(Value))
 
 #### load soil temperature data ####
-soilTemp <- read.csv("../Raw Data/EnviData/iButton Data/SoilTemp_allYear.csv")
+soilTemp <- read.csv(paste0(datFolder, "/Raw Data/EnviData/iButton Data/SoilTemp_allYear.csv"))
 # correct the format of the dat column
 soilTemp$Date <- as.POSIXct(x = soilTemp$Date, tz = "UTC")
 # add a month column
@@ -75,14 +76,14 @@ soilTemp_plot <- left_join(soilTemp_winter, soilTemp_grow)
 # there isn't any data for any Unnamed Creek plots for months 1-4 and 11-12; replace with diamond creek averages?
 
 #### load PRISM data ####
-baseClim <- read.csv("../Raw Data/EnviData/FEWAB_PRISM_data.csv", skip = 10)
+baseClim <- read.csv(paste0(datFolder,"/Raw Data/EnviData/FEWAB_PRISM_data.csv"), skip = 10)
 names(baseClim) <- c("Date", "precip_in", "tMin_F", "tMean_F", "tMax_F", "meanDewPt_F", "VPDmin_hPa", "VPDmax_hPa")
 baseClim$Date <- paste0(baseClim$Date,"-01")
 baseClim$Date <- as.POSIXct(x = baseClim$Date, format = "%Y-%m-%d", tz = "UTC")
 baseClim$Location <- "FEWAFB"
 
 
-SoapClim <- read.csv("../Raw Data/EnviData/Soapstone_PRISM_data.csv", skip = 10)
+SoapClim <- read.csv(paste0(datFolder, "/Raw Data/EnviData/Soapstone_PRISM_data.csv"), skip = 10)
 names(SoapClim) <- c("Date", "precip_in", "tMin_F", "tMean_F", "tMax_F", "meanDewPt_F", "VPDmin_hPa", "VPDmax_hPa")
 SoapClim$Date <- paste0(SoapClim$Date,"-01")
 SoapClim$Date <- as.POSIXct(x = SoapClim$Date, format = "%Y-%m-%d", tz = "UTC")
